e0c66865153d3a2b9496afb71595f2b6d02c316e
Techi-Freki <techifreki@proton.me>
2025-06-12 20:28:25 -0400

    updated color scheme

--- myqtile/colors.py
+++ myqtile/colors.py
@@ -3,17 +3,17 @@ from enum import unique, Enum
 
 @unique
 class Colors(Enum):
-    BLACK = "#001219"
-    DARKBLUE = "#005f73"
-    BLUE = "#0a9396"
-    LIGHTBLUE = "#94d2bd"
-    WHITE = "#e9d8a6"
-    YELLOW = "#ee9b00"
-    ORANGE = "#ca6702"
-    BURNTORANGE = "#bb3e03"
-    RED = "#ae2012"
-    DARKRED = "#9b2226"
+    BLACK = "#09060d"
+    DARKBLUE = "#1e2a50"
+    BLUE = "#484e70"
+    LIGHTBLUE = "#32789c"
+    WHITE = "#feefd2"
+    YELLOW = "#ffac36"
+    ORANGE = "#ca4d15"
+    BURNTORANGE = "#af2b27"
+    RED = "#fe6b71"
+    DARKRED = "#000"
 
     @property
-    def index(self):
+    def index():
         return list(Colors).index(self)
